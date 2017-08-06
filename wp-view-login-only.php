2387c2574242782cbe85189c8019b5130ade376f
Chiilog <purgexpurge@gmail.com>
2017-08-06 16:57:37 +0900

    change version 1.2.1

--- wp-view-login-only.php
+++ wp-view-login-only.php
@@ -5,7 +5,7 @@
  * Description: If you view a website without log in, WordPress redirect to the login page
  * Author: mel_cha
  * Author URI: http://chiilog.com/
- * Version: 1.2
+ * Version: 1.2.1
  * Text Domain: wp-view-login-only
  *
  * @package wp-view-login-only
