199ba068bd1f85e0fcac3cc82e43d54a99c88cc5
Chiaki Obara <obara@ham.works>
2020-07-31 17:07:09 +0900

    Release 1.2.7

--- wp-view-login-only.php
+++ wp-view-login-only.php
@@ -5,7 +5,7 @@
  * Description: If you view a website without log in, WordPress redirect to the login page
  * Author: mel_cha
  * Author URI: https://chiilog.com/
- * Version: 1.2.6
+ * Version: 1.2.7
  * Text Domain: wp-view-login-only
  *
  * @package wp-view-login-only
